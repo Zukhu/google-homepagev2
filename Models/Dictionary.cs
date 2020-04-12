06bdbeba8ee27e62ca4c0c7daee6b7bf63643652
unknown <oerstedico@gmail.com>
2020-04-12 16:33:39 +0200

    Dictionary model

---
+++ Models/Dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace google_homepagev2.Models
+{
+    public class Dictionary
+    {
+        [JsonPropertyName("word")]
+        public int Word { get; private set; }
+
+        [JsonPropertyName("meaning")]
+        public int Meaning { get; private set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Dictionary>(this);
+        
+    }
+}
